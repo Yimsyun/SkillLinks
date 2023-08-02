a0542ff249d267c9cba0f119abb8fbfc17bbf19c
yimseyun <bedals36@gmail.com>
2023-08-02 20:43:03 +0900

    commit

--- skilllink/src/App.js
+++ skilllink/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>이제 시작입니다. 스킬링크</h1>
-        <p>테스트용 </p>
+        <p>테스트용입니다. 이제 시작해볼까용? </p>
       </header>
     </div>
   );
