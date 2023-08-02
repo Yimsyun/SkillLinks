49145ade190ceb97b9ed339a87a631c1cbdba676
kimsunghoon <starcar2@naver.com>
2023-08-02 21:14:24 +0900

    commit

--- skilllink/src/App.js
+++ skilllink/src/App.js
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
         <h1>이제 시작입니다. 스킬링크</h1>
         <p>테스트용입니다. 이제 시작해볼까용? </p>
+        <p>이제 진짜 시작이당~!!</p>
+        <p>이젠 진짜다</p>
       </header>
     </div>
   );
